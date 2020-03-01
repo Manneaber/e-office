b1734d85ffe6a751c08ad34b20b30b4e708d116c
Manneaber <Manneaber@users.noreply.github.com>
2020-03-01 17:10:15 +0700

    Fix Bootstrap dropdown not working

--- web/application/views/bottom.php
+++ web/application/views/bottom.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <script src="<?=base_url('/static/jquery/jquery-3.4.1.min.js')?>"></script>
-<script src="<?=base_url('/static/popper/popper2.min.js')?>"></script>
-<script src="<?=base_url('/static/bootstrap-4.4.1-dist/js/bootstrap.min.js')?>"></script>
+<script src="<?=base_url('/static/bootstrap-4.4.1-dist/js/bootstrap.bundle.js')?>"></script>
 <script src="<?=base_url('/static/app/js/app.js')?>"></script>
 
 </body>
